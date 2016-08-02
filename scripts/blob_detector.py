21057b3b4879fbb0a7f8c124886933ebb2fa2377
Adithya Balaji <adithyabsk@gmail.com>
2016-08-02 09:47:39 -0400

    Add Challenge Detector

--- scripts/blob_detector.py
+++ scripts/blob_detector.py
@@ -5,7 +5,7 @@ import cv2
 import rospy
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
-#import threading
+from skimage.measure import structural_similarity as ssim
 #from racecar_wk3.msg import BlobDetections
 from std_msgs.msg import String
 #from geometry_msgs.msg import Point
@@ -13,6 +13,11 @@ import math
 import time
 import sys
 import RacecarUtilities
+
+SERTAC_IMG = cv2.resize(cv2.imread('sertac.png',0), (100,100), interpolation = cv2.INTER_AREA)
+ARI_IMG = cv2.resize(cv2.imread('ari.png',0), (100,100), interpolation = cv2.INTER_AREA)
+CAT_IMG = cv2.resize(cv2.imread('cat.png',0), (100,100), interpolation = cv2.INTER_AREA)
+RACECAR_IMG = cv2.resize(cv2.imread('racecar.png',0), (100,100), interpolation = cv2.INTER_AREA)
 
 
 class BlobDetector:
@@ -46,6 +51,45 @@ class BlobDetector:
             self.window_thread = RacecarUtilities.StoppableThread(target=self.window_runner)
             self.window_thread.start()
         rospy.loginfo("BlobDetector initialized.")
+    def classify(self, passed_im):
+        """
+        def display_compare_images(imageA, imageB, title):
+            # compute the structural similarity
+            # index for the images
+            s = ssim(imageA, imageB)
+        
+            # setup the figure
+            fig = plt.figure(title)
+            plt.suptitle("SSIM: %.2f" % s)
+        
+            # show first image
+            ax = fig.add_subplot(1, 2, 1)
+            plt.imshow(imageA, cmap = plt.cm.gray)
+            plt.axis("off")
+        
+            # show the second image
+            ax = fig.add_subplot(1, 2, 2)
+            plt.imshow(imageB, cmap = plt.cm.gray)
+            plt.axis("off")
+        
+            # show the images
+            plt.show()
+        """
+        classification = "None"
+        img1 = cv2.resize(cv2.cvtColor(passed_im, cv2.COLOR_BGR2GRAY), (100, 100), interpolation = cv2.INTER_AREA) 
+        img2 = SERTAC_IMG
+
+        imageClasses = SERTAC_IMG, ARI_IMG, CAT_IMG, RACECAR_IMG
+        similarities = []
+        classifications = ["sertac", "ari", "cat", "racecar"]
+        for image in imageClasses:
+            similarities.append(ssim(image, img1))
+            # display compare the images
+            #display_compare_images(image, img1, "Source vs. Compare")
+        if similarities:
+            classification = classifications[similarities.index(max(similarities))]
+
+        return classification
 
     def processImage(self, image_msg):
         im = self.bridge.imgmsg_to_cv2(image_msg)
@@ -56,11 +100,10 @@ class BlobDetector:
             self.find_color(im, "green", cv2.inRange(hsv, np.array([40, 100, 100]), np.array([85, 255, 255])))  # green
             self.find_color(im, "yellow", cv2.inRange(hsv, np.array([20, 150, 150]), np.array([30, 255, 255])))  # yellow
             self.find_color(im, "blue", cv2.inRange(hsv, np.array([100, 115, 55]), np.array([130, 255, 255])))  # blue
-            self.find_color(im, "pink", cv2.inRange(hsv, np.array([135, 80, 90]), np.array([165, 255, 255])))  # pink
-            self.find_faces(im)  # faces
+            self.challenge_color(im, "pink", cv2.inRange(hsv, np.array([135, 80, 90]), np.array([165, 255, 255])))  # pink
         else:
             self.find_color(im, "testing",cv2.inRange(hsv, np.array([self.hl, self.sl, self.vl]), np.array([self.hu, self.su, self.vu])))
-
+            self.challenge_color(im, "challenge testing", np.array([self.hl, self.sl, self.vl]), np.array([self.hu, self.su, self.vu]))  # pink
     def find_color(self, passed_im, label_color, mask):
         if self.isTesting:
             self.image = im
@@ -108,34 +151,58 @@ class BlobDetector:
             #    cv2.drawContours(self.image, approx_contours, -1, (100, 255, 100), 2)
             #else:
             #    cv2.drawContours(im, approx_contours, -1, (100, 255, 100), 2)
+    def challenge_color(passed_im, label_color, mask):
+    if isTesting:
+        image = im
+    contours = cv2.findContours(mask, cv2.cv.CV_RETR_TREE, cv2.cv.CV_CHAIN_APPROX_SIMPLE)[0]
+    crop_img = None
+    for c in contours:
+        area = cv2.contourArea(c)
+        if area < 500: 
+            continue
+        perim = cv2.arcLength(c, True)
+        approx = cv2.approxPolyDP(c, .03*perim, True)
+        if len(approx) == 4:  # rectangle
 
-    def find_faces(self, passed_im):
-        im = passed_im.copy()
-        gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-        faces = self.face_cascade.detectMultiScale(gray, 1.3, 5)
-        found_face = False
-        for (x, y, w, h) in faces:
-            roi_gray = gray[y:y+h, x:x+w]
-            roi_color = im[y:y+h, x:x+w]
-            eyes = self.eye_cascade.detectMultiScale(roi_gray)
-            if len(eyes) == 0: continue
-            found_face = True
-            cv2.rectangle(im, (x, y), (x+w, y+h), (100, 255, 100), 2)
-            eh = eyes[0][3]
-            if eh == 0: continue
-            print "face height: {},  eye height: {}".format(h, eh)
-            if abs(h/eh - 1.5) < 1.5:
-                print "detected karaman"
-                cv2.imwrite("/home/racecar/challenge_photos1/karaman{}.png".format(int(time.clock()*1000)), im)
-                blob_msg = String()
-                blob_msg.data = "professor karaman"
-                self.pub_blobs.publish(blob_msg)
-            elif abs(h/eh - 6) < 3:
-                print "detected ari"
-                cv2.imwrite("/home/racecar/challenge_photos1/ari{}.png".format(int(time.clock()*1000)), im)
-                blob_msg = String()
-                blob_msg.data = "ari"
-                self.pub_blobs.publish(blob_msg)
+            flattenPoints = approx.reshape(4, 2) #Reshape retval of approxPolyDP to just np.ndarry() of points
+            
+            #Routine to find TopLeft Corner of a list of points returned by approxPolyDP            
+            topLeftPoints = flattenPoints.copy()  #Copy over flatten points
+            xTopRemove = topLeftPoints.T[0].argsort()[-2:] #Find the indices of the two points with the largest x values
+            for index in sorted(xTopRemove, reverse=True): #Remove those points
+                topLeftPoints = np.delete(topLeftPoints, index, axis=0)
+            yTopRemove = topLeftPoints.T[1].argsort()[:1]  #Find the index of the point with the largest y value
+            for index in sorted(yTopRemove, reverse=True): #Remove that points
+                topLeftPoints = np.delete(topLeftPoints, index, axis=0)
+            topLeft = topLeftPoints.reshape(2)
+
+            #Routine to find BottomRight Corner of a list of points returned by approxPolyDP            
+            bottomRightPoints = flattenPoints.copy()  #Copy over flatten points
+            xBottomRemove = bottomRightPoints.T[1].argsort()[-2:] #Find the indices of the two points with the largest y values
+            for index in sorted(xBottomRemove, reverse=True): #Remove those points
+                bottomRightPoints = np.delete(bottomRightPoints, index, axis=0)
+            yBottomRemove = bottomRightPoints.T[0].argsort()[:1]  #Find the index of the point with the smallest x value
+            for index in sorted(yBottomRemove, reverse=True): #Remove that points
+                bottomRightPoints = np.delete(bottomRightPoints, index, axis=0)
+            bottomRight = bottomRightPoints.reshape(2)
+
+            x1 = topLeft[0]
+            y2 = topLeft[1]
+            x2 = bottomRight[0]
+            y1 = bottomRight[1]
+            crop_img = passed_im[y1:y2, x1:x2] # Crop from x, y, w, h -> 100, 200, 300, 400
+            classification = classify(crop_img)
+
+            rect_msg = String()
+            rect_msg.data = "{} {}".format(classification, label_color)
+            self.pub_blobs.publish(rect_msg)
+            moments = cv2.moments(c)
+            center = (int(moments['m10']/moments['m00']), int(moments['m01']/moments['m00']))
+            im = passed_im.copy()
+            cv2.putText(im, "{} rectangle".format(label_color), center, cv2.FONT_HERSHEY_PLAIN, 2, (100, 255, 100))
+            cv2.drawContours(im, [approx], -1, (100, 255, 100), 2)
+            cv2.imwrite("/home/racecar/challenge_photos1/{}rectangle{}.png".format(label_color, int(time.clock()*1000)), im)
+            print classification
     def window_runner(self):
         cv2.imshow('HSV', cv2.resize(self.image, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA))
         k = cv2.waitKey(1) & 0xFF
